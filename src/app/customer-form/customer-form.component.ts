23762a980a05a2a92fb0ece52497be391173e3a9
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-22 16:58:51 +0530

    done

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { RestService } from '../_services/rest.service';
+import { CustomerCreateViewModel } from '../_models/app.models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-form',
   templateUrl: './customer-form.component.html',
-  styleUrls: ['./customer-form.component.scss']
+  styleUrls: ['./customer-form.component.scss'],
 })
 export class CustomerFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private restService: RestService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phone: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  get firstName(): FormControl {
+    return this.form.controls['firstName'] as FormControl;
+  }
+
+  get lastName(): FormControl {
+    return this.form.controls['lastName'] as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.form.controls['email'] as FormControl;
+  }
+
+  get phone(): FormControl {
+    return this.form.controls['phone'] as FormControl;
+  }
+
+  onSubmit() {
+    this.restService
+      .post<CustomerCreateViewModel>(
+        'http://localhost:8080/api/v1/customers',
+        this.form.value
+      )
+      .subscribe((res) => {
+        this.router.navigate(['/']);
+      });
+  }
 }
