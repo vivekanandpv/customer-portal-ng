15e177bf8920f99a213062e27ad1c1d39d0e0c30
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-22 16:40:51 +0530

    list

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CustomerViewModel, ServerResponse } from '../_models/app.models';
+import { RestService } from '../_services/rest.service';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
-  styleUrls: ['./customer-list.component.scss']
+  styleUrls: ['./customer-list.component.scss'],
 })
 export class CustomerListComponent implements OnInit {
+  customers$: Observable<CustomerViewModel[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private restService: RestService) {
+    this.customers$ = this.restService
+      .read<ServerResponse<CustomerViewModel[]>>(
+        'http://localhost:8080/api/v1/customers'
+      )
+      .pipe(map((v) => v.body));
   }
 
+  ngOnInit(): void {}
 }
